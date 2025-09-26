b18bedce5eea3538743eedc91e9518bed7400541
Vilson teixeira junior <junior99252@gmail.com>
2025-09-26 14:53:31 -0300

    Lista4
    Exercicio em aula

---
+++ Lista4/src/exe1/Carro.java
@@ -0,0 +1,30 @@
+package exe1;
+public class Carro extends Veiculo {
+    private int portas;
+    public Carro(){
+        super(); // chama o construtor da superclasse
+    }
+    public Carro(String placa, String modelo, double valorBase, int portas) {
+        // chama o construtor da superclasse
+        super(placa, modelo, valorBase);
+        this.portas = portas;
+    }
+    public int getPortas() {
+        return portas;
+    }
+    public void setPortas(int portas) {
+        this.portas = portas;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                "portas=" + portas +
+                super.toString() +
+                '}';
+    }
+    @Override
+    public double calcularImposto(){
+        return this.valorBase * 0.03;
+    }
+}
---
+++ Lista4/src/exe1/Veiculo.java
@@ -0,0 +1,52 @@
+package exe1;
+public class Veiculo {
+    protected String placa;
+    protected String modelo;
+    protected double valorBase;
+
+    public Veiculo() {
+    }
+
+    public Veiculo(String placa, String modelo, double valorBase) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.valorBase = valorBase;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getValorBase() {
+        return valorBase;
+    }
+
+    public void setValorBase(double valorBase) {
+        this.valorBase = valorBase;
+    }
+
+    @Override
+    public String toString() {
+        return "Veiculo{" +
+                "placa='" + placa + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", valorBase=" + valorBase +
+                ", imposto = " + calcularImposto() +
+                '}';
+    }
+    public double calcularImposto(){
+        return this.valorBase * 0.02f;
+    }
+}
